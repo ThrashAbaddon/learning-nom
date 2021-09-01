38b129847771f7bb54a6d88a2f60c4d7a8ef356b
ThrashAbaddon <straca369@gmail.com>
2021-09-01 23:28:10 +0200

    Add parse_separated

--- src/main.rs
+++ src/main.rs
@@ -37,11 +37,27 @@ fn parse_comma_tags<'input: 'tag, 'tag>(
 ) -> impl Parser<&'input str, (&'input str, &'input str), ()> + 'tag {
     move |input: &'input str| {
         let mut parse_tag1 = parse_tag(tag1);
-        let mut parse_comma = parse_tag(", ");
+        let mut parse_separator = parse_tag(", ");
         let mut parse_tag2 = parse_tag(tag2);
 
         let (tail, value1) = parse_tag1.parse(input)?;
-        let (tail, _) = parse_comma.parse(tail)?;
+        let (tail, _) = parse_separator.parse(tail)?;
+        let (tail, value2) = parse_tag2.parse(tail)?;
+        Ok((tail, (value1, value2)))
+    }
+}
+
+/***
+Accepts any parser we want.
+*/
+fn parse_separated<'i>(
+    mut parse_tag1: impl Parser<&'i str, &'i str, ()>,
+    mut parse_separator: impl Parser<&'i str, &'i str, ()>,
+    mut parse_tag2: impl Parser<&'i str, &'i str, ()>,
+) -> impl Parser<&'i str, (&'i str, &'i str), ()> {
+    move |input: &'i str| {
+        let (tail, value1) = parse_tag1.parse(input)?;
+        let (tail, _) = parse_separator.parse(tail)?;
         let (tail, value2) = parse_tag2.parse(tail)?;
         Ok((tail, (value1, value2)))
     }
@@ -49,8 +65,8 @@ fn parse_comma_tags<'input: 'tag, 'tag>(
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse_comma_tags, parse_hello, parse_tag};
-    use nom::Parser;
+    use crate::{parse_comma_tags, parse_hello, parse_separated, parse_tag};
+    use nom::{Err, IResult, Parser};
 
     #[test]
     fn test() {
@@ -71,6 +87,17 @@ mod tests {
             parse_comma_tags("Hello", "World")
                 .parse("Hello, World!!")
                 .unwrap(),
+            ("!!", ("Hello", "World"))
+        )
+    }
+
+    #[test]
+    fn test_parse_separated() {
+        let mut parse_hello_world =
+            parse_separated(parse_tag("Hello"), parse_tag(", "), parse_tag("World"));
+
+        assert_eq!(
+            parse_hello_world.parse("Hello, World!!").unwrap(),
             ("!!", ("Hello", "World"))
         )
     }
